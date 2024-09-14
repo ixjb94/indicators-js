447640de7a0de52ae2fa80fb92c8b2ab10f7f6d3
ixjb94 <ixjb94@gmail.com>
2024-09-14 23:03:06 +0000

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,36 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { readdirSync } from 'fs';
+import { join } from 'path';
 
-export default {
-    input: 'index.js',
-    output: {
-        file: 'dist/index.umd.js',
-        format: 'umd',
-        name: 'indicators',
-        exports: 'named',
+const coreDir = './core';
+const coreFiles = readdirSync(coreDir)
+    .filter(file => file.endsWith('.js'))
+    .reduce((entries, file) => {
+        const name = file.replace('.js', '');
+        entries[name] = join(coreDir, file);
+        return entries;
+    }, {});
+
+export default [
+    {
+        input: 'index.js',
+        output: {
+            file: 'dist/index.umd.js',
+            format: 'umd',
+            name: 'indicators',
+            exports: 'named',
+        },
+        plugins: [resolve(), commonjs()],
     },
-    plugins: [resolve(), commonjs()],
-};
+
+    // Separate
+    {
+        input: coreFiles,
+        output: {
+            dir: 'dist/modules',
+            format: 'esm',
+        },
+        plugins: [resolve(), commonjs()],
+    }
+];
